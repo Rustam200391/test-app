a256fe2590ab59ded8b5492c623d32f40c6204a6
Rustam Guseinov <rg20031991.com>
2023-06-23 13:31:43 +0300

    init test app

---
+++ src/pages/Block.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./style.module.css";
+
+export const Block = () => {
+  return (
+    <>
+      <h1 className={style.block}>Hello</h1>
+    </>
+  );
+};
